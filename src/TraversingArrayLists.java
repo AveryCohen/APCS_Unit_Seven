8f3d3cf730e810780c11b54b52470f567fdedf27
AveryCohen <AveryCohen04@gmail.com>
2022-02-01 09:54:58 -0500

    worked on statelist

--- src/TraversingArrayLists.java
+++ src/TraversingArrayLists.java
@@ -4,6 +4,32 @@ import java.util.ArrayList;
 import java.util.Scanner;
 
 public class TraversingArrayLists {
+    public static ArrayList<String> getStates() {
+        Scanner scan = new Scanner(System.in);
+        ArrayList<String> stateList = new ArrayList<String>();
+        System.out.print("Enter the name of a state or \"Stop\" to quit: ");
+        String state = scan.nextLine();
+        stateList.add(state);
+        while (!(state.equals("Stop"))) {
+            System.out.print("Next state or \"Stop\": ");
+            state = scan.nextLine();
+            stateList.add(state);
+        }
+        stateList.remove(stateList.size() - 1);
+        return stateList;
+    }
+
+
+    public static String createList(ArrayList<String> states) {
+
+        states = getStates();
+        String list = states.get(0);
+        for (int i=1; i < states.size(); i++) {
+            list += " -> " + states.get(i);
+        }
+        return list;
+    }
+
 
     // Please write the two methods for listOfStates in the space below. Uncomment the tests
     // when you are ready to run them.
