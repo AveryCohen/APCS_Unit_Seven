92f9133d656670f036a8a7b9665f9b64140558a4
AveryCohen <AveryCohen04@gmail.com>
2022-02-10 12:30:13 -0500

    worked on merge

--- src/Merge.java
+++ src/Merge.java
@@ -1,11 +1,38 @@
+import java.io.File;
 import java.util.ArrayList;
+import java.util.Scanner;
 
 public class Merge {
 
-    /*
+//    /*
     public static ArrayList<Integer> mergeLists(ArrayList<Integer> listOne, ArrayList<Integer> listTwo) {
-
+        ArrayList<Integer> listMerge = new ArrayList<>();
+        int i =0;
+        while (i<listTwo.size())
+        {
+            if (listOne.get(i) > listTwo.get(i)) {
+                listMerge.add(listOne.get(i));
+                int j = i+1;
+                while (listOne.get(j) > listTwo.get(i) && j <listOne.size()) {
+                    listMerge.add(listOne.get(j));
+                    j++;
+                }
+            }
+                else if (listOne.get(i) < listTwo.get(i)) {
+                listMerge.add(listTwo.get(i));
+                int j = i;
+                while (listOne.get(j) < listTwo.get(i) && j < listOne.size()) {
+                    listMerge.add(listTwo.get(j));
+                    j++;
+                }
+            }
+                    else {
+                        listMerge.add(listOne.get(i));
+                    }
+                    i++;
+        }
+        return listMerge;
     }
 
-     */
+//     */
 }
